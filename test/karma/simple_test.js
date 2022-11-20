2e708b0221769a184fa99fce9cf1dcb3036333b5
Ava Mattie <6314286+ava-cassiopeia@users.noreply.github.com>
2022-11-20 13:35:03 -0700

    Try adding Karma+Jasmine tests

---
+++ test/karma/simple_test.js
@@ -0,0 +1,9 @@
+describe("SimpleSwitch", () => {
+  it("should be available in the browser", () => {
+    expect(SimpleSwitch).toBeTruthy();
+  });
+
+  it("should make the Switch class available", function () {
+    expect(SimpleSwitch.Switch).toBeTruthy();
+  });
+});
